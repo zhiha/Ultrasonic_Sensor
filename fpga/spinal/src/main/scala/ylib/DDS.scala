e1181195cb21f0cb6a89f4ca3fdca3e93bed3d7a
zhiha <yaozhiwei4869@outlook.com>
2022-11-06 09:57:48 +0800

    update

--- fpga/spinal/src/main/scala/ylib/DDS.scala
+++ fpga/spinal/src/main/scala/ylib/DDS.scala
@@ -5,9 +5,9 @@ import spinal.core.sim._
 
 
 case class FmcwDdsConfig(
-                          val phaseResolution: Int = 20,
+                          val phaseResolution: Int = 12,
                           val signalResolution: Int = 16,
-                          val coefResolution:Int = 40,
+                          val coefResolution:Int = 32,
                           val resourceWidth:Int = 64,
                           val chirpTime: Double = 1e-2,
                           val bandwidth: Double = 1e3,
@@ -28,7 +28,6 @@ class DDS(config:FmcwDdsConfig=FmcwDdsConfig()) extends Component {
 
   //Default Parameter
   val fclk = config.fclk
-
   val fc = config.fc
   val chirpTime = config.chirpTime
   val bandwidth = config.bandwidth
@@ -37,37 +36,36 @@ class DDS(config:FmcwDdsConfig=FmcwDdsConfig()) extends Component {
   val signalCount = (chirpTime*fclk).toInt
   val coefFactor = BigDecimal(math.pow(2,config.coefResolution))
 
-
-  val coe_1 = Reg(UInt(config.resourceWidth bits)).allowUnsetRegToAvoidLatch init(((fc*sampleCount/fclk)*coefFactor).toBigInt())
-  val coe_2 = Reg(UInt(config.resourceWidth bits)).allowUnsetRegToAvoidLatch init(((slope*sampleCount/fclk/fclk/2)*coefFactor).toBigInt()) simPublic()
+  val coe_1 = Reg(UInt(config.resourceWidth bits)).allowUnsetRegToAvoidLatch init(((fc*sampleCount/fclk)*coefFactor+(slope*sampleCount/fclk/fclk/2)*coefFactor).toBigInt())
+  val coe_2 = Reg(UInt(config.resourceWidth bits)).allowUnsetRegToAvoidLatch init(((slope*sampleCount/fclk/fclk)*coefFactor).toBigInt()) simPublic()
 
   val cnt = Reg(UInt(32 bits)) init(0) simPublic()
-  val cnt_last = Reg(UInt(32 bits)) init(0) simPublic()
-  val sum = Reg(UInt(32 bits)) init(0)
-  val sub_coe_2 = Reg(UInt(config.resourceWidth bits)) init(0)
+  val step_cur = Reg(UInt(config.resourceWidth bits)) init(0)
   val step_acc = Reg(UInt(config.resourceWidth bits)) init(0)
   val step = Reg(UInt(config.phaseResolution bits)) init(0)
-
   val rom =  Mem(SInt(config.signalResolution bits),initialContent = sinTable)
-
-  cnt_last := cnt
-  cnt := cnt + 1
-  sum := (cnt_last + cnt).resized
-  sub_coe_2 := coe_1+(coe_2*sum).resized
-  step_acc := step_acc + sub_coe_2
-  step := step_acc.fixTo(config.resourceWidth downto (config.coefResolution)).resized
-
   val flag = Reg(Bool()) init(false)
 
-  when(cnt===U(signalCount-1) && ~flag){
+  cnt := cnt + 1
+  step_cur := step_cur + coe_2
+  step_acc := step_acc + step_cur
+
+  when(cnt===0){
+    step_cur := 0
+  }
+
+  when(cnt===1){
+    step_cur := coe_1
+  }
+
+  step := step_acc.fixTo(config.resourceWidth downto (config.coefResolution)).resized
+
+  when(cnt===U(signalCount) && ~flag){
     cnt := 0
-    cnt_last := 0
     flag := True
   }
 
   when(flag){
-    sum := 0
-    sub_coe_2 := 0
     step_acc := 0
     flag := False
   }
@@ -80,8 +78,7 @@ class DDS(config:FmcwDdsConfig=FmcwDdsConfig()) extends Component {
     S((sinValue * (1<<(config.signalResolution-2))).toShort,config.signalResolution bits)
   }
 
-  def delay = 3
-
+  def delay = 4
 
 
 }
